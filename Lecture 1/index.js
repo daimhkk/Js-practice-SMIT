ab877149ab550d8afe72672aeb95560ad6c078b7
daimhkk <daimhussainqk@gmail.com>
2025-11-08 00:04:01 -0800

    Add files via upload

---
+++ Lecture 1/index.js
@@ -0,0 +1,15 @@
+let first ="ali";
+let age = 23;
+let city = true;
+
+console.log(typeof(first));
+console.log(typeof(age));
+console.log(typeof(city));
+
+// let name = prompt("Enter your name:");
+// let age = prompt("Enter your age:");
+
+// let message = 'hello  ${name} you are ${age} years old';
+
+// console.log(message);
+// alert(message);
